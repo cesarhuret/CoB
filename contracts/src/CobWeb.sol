656e5aca077ed2a8056a5cd35c5980ad55341279
Cesar <cesar.huret@gmail.com>
2023-11-18 06:06:12 +0000

    compiled cobweb v1: needs tests
    Co-authored-by: Eason Wu <eason-w@users.noreply.github.com>

--- contracts/src/CobWeb.sol
+++ contracts/src/CobWeb.sol
@@ -1,13 +1,16 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
 import {IMessageRecipient} from "@hyperlane/v3/interfaces/IMessageRecipient.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IChronicle} from "@chronicle/contracts/IChronicle.sol";
+import {IChronicle} from "@chronicle/contracts/IChronicle.sol";
 import {Utils} from "./utils.sol";
-import "@hyperlane/v3/Mailbox.sol";
+import {ISelfKisser} from "./ISelfKisser.sol";
+import {IInterchainGasPaymaster} from "@hyperlane/v3/interfaces/IInterchainGasPaymaster.sol";
+import {IMailbox} from "@hyperlane/v3/interfaces/IMailbox.sol";
 
-contract CobWebV3 is IMessageRecipient {
+contract CobWeb is IMessageRecipient {
     using Utils for uint;
 
     struct Order {
@@ -17,22 +20,41 @@ contract CobWebV3 is IMessageRecipient {
         uint256 amountOut;
         address fromToken;
         address toToken;
-        uint256 fromChain;
-        uint256 toChain;
+        uint32 fromChain;
+        uint32 toChain;
         uint256 deadline;
         uint256 maxSlippage;
         bool filled;
         address[] fillers;
-        mapping(address => uint256) fills;
+        uint256[] fills;
     }
 
     Order[] public pendingIncomingOrders;
+    Order[] private outgoing;
 
     mapping(address => address) tokenToOracle;
 
-    constructor(address mailbox, mapping(address => address) _tokenToOracle) {
+    uint256[] public chains;
+
+    constructor(
+        address mailbox,
+        address selfKisser,
+        address[] memory _tokens,
+        address[] memory _oracles,
+        uint256[] memory _chains
+    ) {
+        require(_tokens.length == _oracles.length, "Length mismatch");
+
         _mailbox = mailbox;
-        tokenToOracle = _tokenToOracle;
+        chains = _chains;
+
+        for (uint256 i = 0; i < _oracles.length; i++) {
+            ISelfKisser(selfKisser).selfKiss(_oracles[i], address(this));
+        }
+
+        for (uint256 i = 0; i < _tokens.length; i++) {
+            tokenToOracle[_tokens[i]] = _oracles[i];
+        }
     }
 
     address immutable _mailbox;
@@ -46,88 +68,133 @@ contract CobWebV3 is IMessageRecipient {
         _;
     }
 
-    function bridge(Order memory order) external overrides {
+    function bridge(
+        uint256 sourceId,
+        uint256 amountIn,
+        uint256 amountOut,
+        address fromToken,
+        address toToken,
+        uint32 fromChain,
+        uint32 toChain,
+        uint256 deadline,
+        uint256 maxSlippage
+    ) external {
+        Order memory order = Order(
+            sourceId,
+            msg.sender,
+            amountIn,
+            amountOut,
+            fromToken,
+            toToken,
+            fromChain,
+            toChain,
+            deadline,
+            maxSlippage,
+            false,
+            new address[](0),
+            new uint256[](0)
+        );
+
+        uint256 fromPrice = IChronicle(tokenToOracle[order.fromToken]).read();
+        uint256 toPrice = IChronicle(tokenToOracle[order.toToken]).read();
+
         if (order.fromToken != order.toToken) {
             // oracle provided rate
-            order.amountOut = 0;
+            order.amountOut = (fromPrice * order.amountIn) / toPrice;
         } else {
             order.amountOut = order.amountIn;
         }
 
-        order.filled = false;
+        for (uint256 k = 0; k < chains.length; k++) {
+            Order[] memory ordersToBroadcast;
 
-        Order[] memory ordersToBroadcast;
+            for (uint i = 0; i < pendingIncomingOrders.length; i++) {
+                uint256 newAmountOut = (pendingIncomingOrders[i].amountIn *
+                    fromPrice) / toPrice;
 
-        for (uint i = 0; i < pendingIncomingOrders.length; i++) {
-            // uint256 deltaPriceSlippage = pendingIncomingOrders[i].am -
-            //     order.maxSlippage;
-            // MISSING: slippage check
-            if (
-                pendingIncomingOrders[i].fromToken == order.toToken &&
-                pendingIncomingOrders[i].toToken == order.fromToken &&
-                pendingIncomingOrders[i].fromChain == order.toChain &&
-                pendingIncomingOrders[i].toChain == order.fromChain &&
-                pendingIncomingOrders[i].filled == false &&
-                pendingIncomingOrders.deadline < block.timestamp
-            ) {
-                uint256 deltaFilledIn = Utils.min(
-                    order.amountIn,
-                    pendingIncomingOrders[i].amountOut
-                );
-
-                uint256 deltaFilledOut = Utils.min(
-                    order.amountOut,
-                    pendingIncomingOrders[i].amountIn
-                );
-
-                // we fill whatever we can
-                IERC20(order.fromToken).transferFrom(
-                    msg.sender,
-                    pendingIncomingOrders[i].sender,
-                    deltaFilledIn
-                );
-
-                order.amountIn -= deltaFilledIn;
-                order.amountOut -= deltaFilledOut;
-
-                pendingIncomingOrders[i].fillers.push(msg.sender);
-                pendingIncomingOrders[i].fills[msg.sender] += deltaFilledIn;
+                if ((pendingIncomingOrders[i].amountOut > newAmountOut)) {
+                    uint256 slippage = pendingIncomingOrders[i].amountOut -
+                        newAmountOut;
+                    if (slippage > pendingIncomingOrders[i].maxSlippage) {
+                        // we don't fill this order
+                        continue;
+                    }
+                }
 
                 if (
-                    pendingIncomingOrders[i].amountIn == 0 &&
-                    pendingIncomingOrders[i].amountOut == 0
+                    pendingIncomingOrders[i].fromToken == order.toToken &&
+                    pendingIncomingOrders[i].toToken == order.fromToken &&
+                    pendingIncomingOrders[i].fromChain == order.toChain &&
+                    pendingIncomingOrders[i].toChain == order.fromChain &&
+                    pendingIncomingOrders[i].filled == false &&
+                    pendingIncomingOrders[i].deadline < block.timestamp &&
+                    pendingIncomingOrders[i].fromChain == chains[k]
                 ) {
-                    pendingIncomingOrders[i].filled = true;
-                    ordersToBroadcast.push(pendingIncomingOrders[i]);
+                    uint256 deltaFilledIn = Utils.min(
+                        order.amountIn,
+                        pendingIncomingOrders[i].amountOut
+                    );
+
+                    uint256 deltaFilledOut = Utils.min(
+                        order.amountOut,
+                        pendingIncomingOrders[i].amountIn
+                    );
+
+                    // we fill whatever we can
+                    IERC20(order.fromToken).transferFrom(
+                        msg.sender,
+                        pendingIncomingOrders[i].sender,
+                        deltaFilledIn
+                    );
+
+                    order.amountIn -= deltaFilledIn;
+                    order.amountOut -= deltaFilledOut;
+
+                    pendingIncomingOrders[i].fillers.push(msg.sender);
+                    pendingIncomingOrders[i].fills.push(deltaFilledIn);
+
+                    if (
+                        pendingIncomingOrders[i].amountIn == 0 &&
+                        pendingIncomingOrders[i].amountOut == 0
+                    ) {
+                        pendingIncomingOrders[i].filled = true;
+                        ordersToBroadcast[
+                            ordersToBroadcast.length
+                        ] = pendingIncomingOrders[i];
+                    }
                 }
             }
+
+            if (order.toChain == chains[k]) {
+                if (!order.filled && order.amountIn != 0) {
+                    // we transfer our pending origin to the contract
+                    IERC20(order.fromToken).transferFrom(
+                        msg.sender,
+                        address(this),
+                        order.amountIn
+                    );
+                }
+
+                ordersToBroadcast[ordersToBroadcast.length] = order;
+
+                // broadcast orders
+            }
+            if (ordersToBroadcast.length > 0) {
+                _broadcast(ordersToBroadcast);
+            }
         }
-
-        if (!order.filled && order.amountIn != 0) {
-            // we transfer our pending origin to the contract
-            IERC20(order.fromToken).transferFrom(
-                msg.sender,
-                address(this),
-                order.amountIn
-            );
-        }
-
-        ordersToBroadcast.push(order);
-
-        // broadcast orders
-        _broadcast(ordersToBroadcast);
     }
 
     function _broadcast(Order[] memory orders) private returns (bytes32) {
         bytes32 messageId = IMailbox(_mailbox).dispatch(
-            _destinationChainId,
-            _addressToBytes32(_bridgeOnDestinationChain),
+            orders[0].toChain,
+            _addressToBytes32(address(this)),
             bytes(abi.encode(orders))
         );
 
         igp.payForGas{value: msg.value}(
             messageId, // The ID of the message that was just dispatched
-            _destinationChainId, // The destination domain of the message
+            orders[0].toChain, // The destination domain of the message
             200000, // 100k gas to use in the recipient's handle function
             msg.sender // refunds go to msg.sender, who paid the msg.value
         );
@@ -135,35 +202,37 @@ contract CobWebV3 is IMessageRecipient {
         return messageId;
     }
 
-    function defaultToBridge(uint256 orderSourceId) {
+    function defaultToBridge(uint256 orderSourceId) public view {
         if (
             pendingIncomingOrders[orderSourceId].filled == false &&
             pendingIncomingOrders[orderSourceId].deadline > block.timestamp
         ) {
             // we don't cob anymore and just bridge
+            // swap token with 1inch
+            // bridge with Hyperlane(?)
         }
     }
 
     function handle(
         uint32 _origin,
-        bytes32 _sender,
+        bytes32,
         bytes calldata _body
-    ) external onlyMailbox {
-        (orders) = abi.decode(_body, Order[]);
+    ) external payable override onlyMailbox {
+        Order[] memory orders = abi.decode(_body, (Order[]));
 
         // handle transfers for filled orders
         for (uint i = 0; i < orders.length; i++) {
-            if (orders[i].filled == true) {
+            if (orders[i].filled == true && orders[i].toChain == _origin) {
                 // we transfer to the person who filled our order
                 for (uint j = 0; j < orders[i].fillers.length; j++) {
-                    if (orders[i].fills[orders[i].fillers[j]] > 0) {
+                    if (orders[i].fills[j] > 0) {
                         IERC20(orders[i].fromToken).transfer(
                             orders[i].fillers[j],
-                            orders[i].fills[orders[i].fillers[j]]
+                            orders[i].fills[j]
                         );
                     }
 
-                    delete orders[i].fills[orders[i].fillers[j]];
+                    delete orders[i].fills[j];
                     // deleting so that in case we have multiple fills from the same address
                     // we don't send them the same amount twice
                     // the fillers array might have duplicates
@@ -178,13 +247,13 @@ contract CobWebV3 is IMessageRecipient {
     }
 
     function removePendingOrder(uint256 sourceId) public {
-        require(index < pendingIncomingOrders.length, "Index out of bounds");
+        require(sourceId < pendingIncomingOrders.length, "Index out of bounds");
 
         // Save the element to be moved to the end
-        uint elementToMove = pendingIncomingOrders[index];
+        Order storage elementToMove = pendingIncomingOrders[sourceId];
 
-        // Shift elements after 'index' up by one position
-        for (uint i = index; i < pendingIncomingOrders.length - 1; i++) {
+        // Shift elements after 'sourceId' up by one position
+        for (uint i = sourceId; i < pendingIncomingOrders.length - 1; i++) {
             pendingIncomingOrders[i] = pendingIncomingOrders[i + 1];
         }
 
@@ -193,5 +262,9 @@ contract CobWebV3 is IMessageRecipient {
 
         // Remove the last element
         pendingIncomingOrders.pop();
+    }
+
+    function _addressToBytes32(address _addr) internal pure returns (bytes32) {
+        return bytes32(uint256(uint160(_addr)));
     }
 }
